9100c78348277a7b46b7f37e840700018a5c066c
Viola <violachandra2806@gmail.com>
2025-10-24 09:21:54 +0700

    Something happened, i fixed bugs 24/10/2025 update

--- appname/routes.py
+++ appname/routes.py
@@ -2,10 +2,12 @@ from flask import request, jsonify
 from appname import app
 
 # IMPORT FUNCTION
-from appname.functions.user_details import *
-from appname.functions.user_login import *
+from appname.datas.user_details import *
+from appname.datas.user_login import *
+from appname.datas.user_friends import *
+from appname.datas.friend_request import *
 from appname.functions.user_register import *
-
+from appname.functions.add_friend import *
 
 # GET USER DETAIL
 @app.route("/chattingapp/getuserdetails", methods=["GET"])
